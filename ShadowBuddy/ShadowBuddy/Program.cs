d48bfd98b70f3fcd7931022f824bed2fc5e49091
Fedor Tikhonin <ftikhonin@yandex.ru>
2023-05-06 00:50:12 +0200

    init commit

---
+++ ShadowBuddy/ShadowBuddy/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace ShadowBuddy;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ConfigureBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder ConfigureBuilder(string[] args)
+    {
+        var builder = Host.CreateDefaultBuilder(args);
+        return builder;
+    }
+
+    private static bool IsEnvironment(string environment) =>
+        Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == environment;
+}
---
+++ ShadowBuddy/ShadowBuddy/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Calzolari.Grpc.AspNetCore.Validation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShadowBuddy;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration) => _configuration = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        
+    }
+}
